4fb37a232b25eec4476369eef29a9faec4d64e73
Marsellahlavine <lavineshikanga2019@gmail.com>
2022-07-04 15:30:44 -0700

    alphabt

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,9 +7,11 @@
  */
 int main(void)
 {
-int num;
-for (num = 0; num < 10; num++)
-printf("%d", num);
-printf("\n");
+char letter;
+for (letter ='a'; letter <= 'z'; letter++)
+putchar(letter);
+for (letter ='A'; letter <= 'Z'; letter++)
+putchar(letter);
+putchar('\n');
 return (0);
 }
