01033a532d52a35b0961acdcee500213362f8b52
Marsellahlavine <lavineshikanga2019@gmail.com>
2022-07-04 12:58:49 -0700

    positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,11 +17,11 @@ if (n > 0)
 {
 	printf("%d is positive\n", n);
 }
-if (n= =0)
+ else if (n= =0)
 {	
         printf("%d is zero\n", n);
 }
-if (n < 0)
+ else if (n < 0)
 {	
 	printf("%d is negative\n", n);
 }					
