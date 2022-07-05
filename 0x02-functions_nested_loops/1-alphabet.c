091b47b44003a49fa2146ca96121fbbbee98ed29
Marsellahlavine <lavineshikanga2019@gmail.com>
2022-07-05 13:26:23 -0700

    alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+nclude "main.h"
+
+/**
+ * main - print _alphabet -> prints the lowercase alphabets
+ *  
+ * Return: Always 0.
+ */
+
+void print _alphabet(void)
+{
+	    int j;
+	    for (j= 'a'; j<= 'z'; j++)
+	    {
+		    putchar(j);
+	    }
+	    putchar('\n');
+
+	        return (0);
+}
