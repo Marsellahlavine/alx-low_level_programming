061c63d4c688289b216dcf31a3c120a16b70f10e
Marsellahlavine <lavineshikanga2019@gmail.com>
2022-07-05 14:35:16 -0700

    x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 -> prints the alphabets in lowercase
+ */
+
+int print_alphabet_x10(void)
+{
+int j;
+int x;
+
+for (x = 0; x < 10; x++)
+{
+putchar(j);
+}
+putchar('\n');
+	    
+}
