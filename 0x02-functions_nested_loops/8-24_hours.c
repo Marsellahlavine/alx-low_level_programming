0658e716cb762a2441644f02ea40e5725a5d47ec
Marsellahlavine <lavineshikanga2019@gmail.com>
2022-07-05 16:24:51 -0700

    24-hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,6 +5,7 @@
 void jack_bauer(void)
 {
 int i, j;
+
 for (i = 0; i < 24; i++)
 {
 for (j = 0; j < 60; j++)
@@ -14,10 +15,10 @@ if (i < 10)
 _putchar('0');
 _putchar(i + '0')
 }
-	else if( i >= 10)
+	else if(i >= 10)
 {
-	_putchar((i/10) + ;0;);
-	_putchar((i % 10 + '0');
+	_putchar((i / 10) + '0');
+	_putchar((i % 10)+ '0');
 }	
 if (j < 10)
 {
@@ -28,8 +29,8 @@ _putchar(j + '0');
 else if (j > 10)
 {
 _putchar(':');
-_putchar(j / 10) + '0');
-_putchar(j % 10) + '0');
+_putchar((j / 10) + '0');
+_putchar((j % 10) + '0');
 }
 }
 putchar('\n');
