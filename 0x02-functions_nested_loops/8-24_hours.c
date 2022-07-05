3426dfa5cf744add2b59f645aaffa66974783a1f
Marsellahlavine <lavineshikanga2019@gmail.com>
2022-07-05 16:31:50 -0700

    24-hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -15,10 +15,10 @@ if (i < 10)
 _putchar('0');
 _putchar(i + '0');
 }
-	else if(i >= 10)
+else if(i >= 10)
 {
-	_putchar((i / 10) + '0');
-	_putchar((i % 10)+ '0');
+_putchar((i / 10) + '0');
+_putchar((i % 10)+ '0');
 }	
 if (j < 10)
 {
