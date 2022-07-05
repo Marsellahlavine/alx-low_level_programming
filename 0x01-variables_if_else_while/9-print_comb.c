fa5265af7bc0051f42e9ca8ab59be1ed2d812964
Marsellahlavine <lavineshikanga2019@gmail.com>
2022-07-04 17:04:33 -0700

    comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 int num;
-for (num =0; num <= 9; num++)
+for (num = 0; num <= 9; num++)
 {
 putchar((num % 10) + '0')
 if (num == 9)
@@ -17,5 +17,5 @@ putchar(',');
 putchar('');
 }
 putchar('\n');
-return (0)
+return (0);
 }
