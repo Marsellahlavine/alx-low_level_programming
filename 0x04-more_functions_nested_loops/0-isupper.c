c2b4fc1202c4abbd0ee7f39a22c3d02e03a9c9d5
Marsellahlavine <lavineshikanga2019@gmail.com>
2022-07-07 14:08:47 -0700

    i supper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,14 +3,9 @@
 /**
  * _isupper -> this is a function to check upper case alphabet
  * @c: an integer paramer
- *  Return: success
+ * Return: success
  */
 int _isupper(int c)
 {
-if (c >= 'A' && c <= 'Z')
-return (1);
-return (0);
-
-return (c >= 65 && c <= 90);
 return (c >= 'A' && c <= 'Z');
 }
