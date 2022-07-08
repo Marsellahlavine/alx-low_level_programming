14a588aa3c434b88dc2c221e642e187a60da4014
Marsellahlavine <lavineshikanga2019@gmail.com>
2022-07-08 13:43:24 -0700

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,4 +1,18 @@
 #include "main.h"
 
 /**
- *
+ * more numbers - printing more numbers
+ */
+void more numbers(void)
+{
+int x, y;
+
+for (x = 0; x < 10; x++)
+{
+for (y = 0; y < 15; y++)
+{
+_putchar(y + '0');
+}
+_putchar('\n');
+}
+}
