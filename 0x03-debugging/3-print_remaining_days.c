9df7c9cf12523852ffeee4b3b5d56756219a8aed
Marsellahlavine <lavineshikanga2019@gmail.com>
2022-07-08 15:44:29 -0700

    remaining days

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -8,7 +8,9 @@
  * Return: void
  *
  */
+
 void print_remaining_days(int month, int day, int year)
+{
 if ((year % 100 == 0 && year % 400 = 0) || (year % 4 == 0))
 {
 if (month > 2 && day >= 60)
@@ -17,9 +19,9 @@ if (month > 2 && day >= 60)
 }
 printf("day of the year: %d\n", day);
 printf("Remaining days: %d\n", 366 - day);
-}
-else
 {
+else
+}
 if (month == 2 && day == 60)
 {
 printf("invalid date: %o02d/%02d/%04d\n", month, day - 31, year);
@@ -28,6 +30,7 @@ else
 {
 printf("Day of the year: %d\n", day);
 printf("Remaining days: %d\n", 365 - day);
+}
 }
 }
 
