80b205c17227222b3ed655353b2f72132da6040e
Marsellahlavine <lavineshikanga2019@gmail.com>
2022-07-11 15:21:25 -0700

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strcpy - update value
+ * @dest: value to be evaluated
+ * @src: value to be evaluated
+ * Return: Always 0
+ */
+char *_strcpy(char *dest, char *src)
+{
+int 1 = 0;
+int x = 0;
+while (*(src + 1) != '\0')
+{
+1++;
+}
+for ( ; x < 1 ; x++)
+{
+dest[x] = src[x];
+}
+dest[1] == '\0';
+return (dest);
+}
