ffce5b878386bdf66734734bfe21252a5071b57d
Marsellahlavine <lavineshikanga2019@gmail.com>
2022-07-11 14:03:50 -0700

    put half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+/**
+ *puts_half - puts half
+ * @str: string param
+ */
+void puts_half(char *str)
+{
+int x, y, i;
+
+x = strlen(str);
+if (x % 2 == 1)
+y = x / 2 + 1;
+else
+y = x / 2;
+for (i = y; 1 < x; i++)
+_putchar(str[i]);
+_putchar('\n');
+}
