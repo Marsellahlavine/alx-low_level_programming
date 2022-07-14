0ee866af2236742eb06e47e1c49711d7e6235d06
Marsellahlavine <lavineshikanga2019@gmail.com>
2022-07-14 14:29:19 -0700

    string_toupper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -3,7 +3,7 @@
 /**
  * string_toupper - changes all lowercase letters of a string to uppercase.
  * @c: pointer to the string
- * Return: the converted string
+ * Return: converted string
  */
 char *string_toupper(char *c)
 {
@@ -13,7 +13,7 @@ i = 0;
 while (c[i] != '\0')
 {
 if (c[i] >= 'a' && c[i] <= 'z')
-c([i] -= 'a' - 'A';
+c[i] -= 'a' - 'A'; 
 i++;
 }
 return (c);
