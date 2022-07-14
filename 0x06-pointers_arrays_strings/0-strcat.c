ad729c97f2b0f602404b350f15cebead22664ff5
Marsellahlavine <lavineshikanga2019@gmail.com>
2022-07-14 11:35:05 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,12 +1,11 @@
-#include "MAIN_H"
+#include "main.h"
 
 /**
  * _strcat - concatenates the string pointed to by @src
  * to the end of the string pointed to by @dest
  * @dest: string that will be appended
  * @src: string to be concatenated upon
- *
- * Return:returns pointer to @dest
+ * Return: returns pointer to @dest
  */
 
 char *_strcat(char *dest, char *src)
