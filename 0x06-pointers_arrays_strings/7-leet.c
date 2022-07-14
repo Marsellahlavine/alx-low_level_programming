b8a3ff6c4f51b146c08e1d66db0d6a670882c4b5
Marsellahlavine <lavineshikanga2019@gmail.com>
2022-07-14 15:13:42 -0700

    leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,26 +1,22 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * leet - encodes a string into leetspeek.
- * @s: pointer to input string
- * Return: returns pointer to leets peekstring
+ * leet - encodes a string into 1337
+ * @s: string to encode
+ * Return: address of s
  */
 char *leet(char *s)
 {
 int i, j;
-char subs[] = "aAeEoOtTlL";
-char le[] = "43071";
-
-i = 0;
-for (i = 0; s[i] != '\0'; i++)
+char a[] = "aAeEoOtTlL";
+char b[] = "4433007711";
+for (i = 0; *(s + i); i++)
 {
-for (j = 0; subs[j] != '\0'; j++
-if (s[i] == subs[j])
-
-s[i] == subs[j])
-
-s[i] = le[j / 2];
+for (j = 0; j <= 9; j++)
+{
+if (a[j] == *(s + i))
+*(s + i) = b[j];
+}
 }
 return (s);
 }
