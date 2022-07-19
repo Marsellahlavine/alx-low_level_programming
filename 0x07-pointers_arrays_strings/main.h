71057492c6b5c036e0d0c100f5f145d4fad9384b
Marsellahlavine <lavineshikanga2019@gmail.com>
2022-07-19 14:57:04 -0700

    memset

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,9 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 int _putchar(char c);
-char *memset(char *s, char b, unsigned int n);
-char *memcpy(char *dest, char src, unsigned int n);
-char *strchr(char *s, char c);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char src, unsigned int n);
+char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
