ae2725cb14a83a229011f606fef6fc1a3cba7d33
Marsellahlavine <lavineshikanga2019@gmail.com>
2022-07-19 14:35:52 -0700

    memset

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
 /**
+ 
  * print_number - prints an integer
  * @n: input integer
  * Return: no return
---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+=#include "main.h"
+#include <stdio.h>
+/**
+ * _memset - fills memory with constant byte
+ * @s: source string
+ * @b: the constant byte for filling
+ * @n: length of buffer
+ * Return: new string
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int i = 0;
+
+while (i < n)
+{
+*(s + i) = b;
+i++
+}
+return (s);
+}
