b8fe93f0c42e4f5c82ef0da6a5c48472f2dbb6dd
Marsellahlavine <lavineshikanga2019@gmail.com>
2022-07-19 16:33:07 -0700

    print chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -2,10 +2,10 @@
 /**
  * print_chessboard - prints the chessboard
  * @a: array of pointers
- *
+ * 
  * Return: void
  */
-void print_chessboard(char (*a)[8])
+void print_chessboard(char (a)[8])
 {
 int i, j;
 
