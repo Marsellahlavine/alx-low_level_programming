7d586880f492f44d404e7b111f9bb25ef6224e12
Marsellahlavine <lavineshikanga2019@gmail.com>
2022-07-27 14:14:33 -0700

    mul

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - this is a function t print its name
+ * @argc: argc parameter
+ * @argv: an array of a command listed
+ * Return: 0 for success
+ */
+int main(int argc, char *argv[])
+{
+int result = 0, num1, num2;
+
+if (argc != 3)
+{
+printf("%s\n", "Error");
+return (1);
+}
+num1 = atoi(argv[1]);
+num2 = atoi(argv[2]);
+result = num * num2;
+
+printf("%d\n", result);
+return (0);
+}
