49eba572e990989b823e4f5d0dfb8c76b2bd2066
Marsellahlavine <lavineshikanga2019@gmail.com>
2022-07-29 12:49:38 -0700

    realloc

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+int find_len(char *str);
+char *create_array(int size);
+char *iterate_zeroes(char *str);
+void get_prod(char *prod, char *mult, int digit, int zeroes);
+void add_nums(char *final_prod, char *next_prod, int next_len);
+
+/**
+ * find_len - Findsthe length of a string
+ * @str: The string to be measured
+ *
+ * Return: The length of the string
+ */
+int find_len(char *str)
+{
+int len = 0;
+
+while (*str++)
+len++
